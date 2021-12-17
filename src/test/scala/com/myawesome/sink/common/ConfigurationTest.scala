66913977b52043274e6bcd0b62e208823d8a159b
Kyriakos Sideris <k.sideris@gmail.com>
2021-12-17 14:36:57 +0200

    Reformated configuration testing

--- src/test/scala/com/myawesome/sink/common/ConfigurationTest.scala
+++ src/test/scala/com/myawesome/sink/common/ConfigurationTest.scala
@@ -3,34 +3,48 @@ package com.myawesome.sink.common
 import org.scalatest.flatspec.AsyncFlatSpec
 
 class ConfigurationTest extends AsyncFlatSpec {
-  it should "get configuration from environment" in {
-
-    // Environment variable SERVICE_FOO is set in build.sbt
-    case class A() extends Configuration
-    val a = A()
-    assert(a.getConfigFromEnv("service").get("service.foo").contains("test"))
-    assert(!a.getConfigFromEnv("service").contains("service.bar"))
-  }
-
   it should "get configuration from file" in {
 
     // Configuration properties are set in application.properties file
     case class A() extends Configuration
-    val a = A()
-    assert(a.getConfigFromFile("service").get("service.foo").contains("localhost:8000"))
-    assert(a.getConfigFromFile("service").get("service.url").contains("http://localhost:8080"))
-    assert(a.getConfigFromFile("service").get("service.very.long.path").contains("false"))
-    assert(!a.getConfigFromFile("service").contains("service.bar"))
+    val conf = A().getConfigFromFile("service")
+
+    assert(conf.get("service.foo").contains("localhost:8000"))
+    assert(conf.get("service.url").contains("http://localhost:8080"))
+    assert(conf.get("service.very.long.path").contains("false"))
+    assert(!conf.contains("service.bar"))
   }
 
   it should "get configuration from system properties" in {
 
     // System properties are set in build.sbt
     case class A() extends Configuration
-    val a = A()
-    println("a.getConfigFromEnv: " + a.getConfigFromProps("service").mkString("\n"))
+    val conf = A().getConfigFromProps("service")
 
-    assert(a.getConfigFromProps("service").get("service.cli").contains("false"))
-    assert(!a.getConfigFromProps("service").contains("service.bar"))
+    assert(conf.get("service.cli").contains("false"))
+    assert(!conf.contains("service.bar"))
+  }
+
+  it should "get configuration from environment" in {
+
+    // Environment variable SERVICE_FOO is set in build.sbt
+    case class A() extends Configuration
+    val conf = A().getConfigFromEnv("service")
+
+    assert(conf.get("service.foo").contains("test"))
+    assert(!conf.contains("service.bar"))
+  }
+
+  it should "get all configuration" in {
+
+    // Gather all configuration are set in build.sbt
+    case class A() extends Configuration
+    val conf = A().getAllConfig("service")
+
+    assert(conf.get("url").contains("http://localhost:8080"))
+    assert(conf.get("very.long.path").contains("false"))
+    assert(conf.get("cli").contains("false"))
+    assert(conf.get("foo").contains("test"))
+    assert(!conf.contains("bar"))
   }
 }
